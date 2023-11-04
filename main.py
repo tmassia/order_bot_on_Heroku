7cdc108def7c5b869c8a40f6330ede45ce1fe7a0
tmassia <taniamassya@gmail.com>
2023-11-04 15:56:06 +0200

    Updated files for Heroku deployment - timeout polling

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ async def main():
     dp.include_router(start_router)
     dp.include_router(bdika_gilui_router)
     # Start polling for updates
-    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types(), timeout=60)
 
 
 # Define an echo function to respond when a message is not understood
