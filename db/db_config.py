8eb36fae956c56909f9471ccba309dc92423739e
tmassia <taniamassya@gmail.com>
2023-11-04 15:09:52 +0200

    Updated files for Heroku deployment

--- db/db_config.py
+++ db/db_config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("MY_DATABASE_URL")
 
 engine = create_async_engine(DATABASE_URL, echo=True, future=True)
 AsyncSessionLocal = sessionmaker(
