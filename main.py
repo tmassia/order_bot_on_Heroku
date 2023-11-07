3f294876879e4d3fb39c461fa81c636851cba982
tmassia <taniamassya@gmail.com>
2023-11-07 20:32:59 +0200

    webhook 1.6

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
 # Настройки веб-сервера
 WEB_SERVER_HOST = '0.0.0.0'  # Слушать все сетевые интерфейсы
 WEB_SERVER_PORT = os.environ.get('PORT')  # Порт, предоставленный Heroku
-WEBHOOK_SECRET_PATH = f"/{BOT_TOKEN}"  # Уникальный путь для вебхука
+WEBHOOK_SECRET_PATH = f"{BOT_TOKEN}"  # Уникальный путь для вебхука
 BASE_WEBHOOK_URL = f"https://{HEROKU_APP_NAME}.herokuapp.com/{WEBHOOK_SECRET_PATH}"
 
 # Создаем роутер
