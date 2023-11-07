98c076f2e90a9fe013ecf1e18425fb3b91e2c93a
tmassia <taniamassya@gmail.com>
2023-11-07 20:01:49 +0200

    webhook 1.1

--- main.py
+++ main.py
@@ -20,14 +20,14 @@ from handlers.bdika_gilui_handlers import bdika_gilui_router
 load_dotenv()
 
 # Получаем токен бота и имя приложения Heroku из переменных окружения
-TOKEN = os.getenv('BOT_TOKEN')
+BOT_TOKEN = os.getenv('BOT_TOKEN')
 HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
 
 # Настройки веб-сервера
 WEB_SERVER_HOST = '0.0.0.0'  # Слушать все сетевые интерфейсы
 WEB_SERVER_PORT = int(os.getenv('PORT', 5000))  # Порт, предоставленный Heroku
-WEBHOOK_SECRET_PATH = f"/{TOKEN}"  # Уникальный путь для вебхука
-BASE_WEBHOOK_URL = f"https://{HEROKU_APP_NAME}.herokuapp.com{WEBHOOK_SECRET_PATH}"
+# WEBHOOK_SECRET_PATH = f"/{TOKEN}"  # Уникальный путь для вебхука
+BASE_WEBHOOK_URL = f"https://{HEROKU_APP_NAME}.herokuapp.com{BOT_TOKEN}"
 
 # Создаем роутер
 router = Router()
@@ -37,7 +37,7 @@ router = Router()
 async def on_startup(bot: Bot) -> None:
     # If you have a self-signed SSL certificate, then you will need to send a public
     # certificate to Telegram
-    await bot.set_webhook(f"{BASE_WEBHOOK_URL}{WEBHOOK_SECRET_PATH}")
+    await bot.set_webhook(BASE_WEBHOOK_URL)
 
 
 def main() -> None:
@@ -51,7 +51,7 @@ def main() -> None:
     # Register startup hook to initialize webhook
     dp.startup.register(on_startup)
     # Initialize Bot instance with a default parse mode which will be passed to all API
-    bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+    bot = Bot(BOT_TOKEN, parse_mode=ParseMode.HTML)
     # Create aiohttp.web.Application instance
     app = web.Application()
     # Create an instance of request handler,
@@ -62,7 +62,7 @@ def main() -> None:
         bot=bot
     )
     # Register webhook handler on application
-    webhook_requests_handler.register(app, path=WEBHOOK_SECRET_PATH)
+    webhook_requests_handler.register(app, path=BOT_TOKEN)
     # Mount dispatcher startup and shutdown hooks to aiohttp application
     setup_application(app, dp, bot=bot)
     # And finally start webserver
